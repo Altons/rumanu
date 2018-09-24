34efd6aaae1d75a17cbe0dad199d448b88742e97
Alberto Negron <albertonegron@gmail.com>
2018-09-24 20:19:23 +0100

    add name's specs and alphabet

--- lib/rumanu.rb
+++ lib/rumanu.rb
@@ -1,22 +1,32 @@
 require "rumanu/version"
-
+require "rumanu/helpers"
+require "rumanu/keys"
+require "rumanu/alphabets"
 module Rumanu
   class Numerology
-    attr_accessor :name
+    attr_reader :name, :alphabet
+
     def initialize(name,dob)
       @name = name
       @dob = dob
-      validate_name!
-    end
-    def name=(name)
+      @alphabet = PY_ALPHABET
       validate_name!
     end
 
+    def name=(name)
+      validate_name! if name.empty?
+      validate_name! unless name.is_a? String
+      @name = name
+    end
+
+    # Private methods
 
     def validate_name!
       raise ArgumentError.new("Name must be a string") unless name.is_a? String
       raise ArgumentError.new("Name can't be blank") unless name.length > 0
     end
+
+    private :validate_name!
 
   end
 end
--- spec/rumanu_spec.rb
+++ spec/rumanu_spec.rb
@@ -1,18 +1,29 @@
 RSpec.describe Rumanu do
-  let(:entity) {Rumanu::Numerology.new("John Lennon","09-10-1940")}
-  let(:entity2) {Rumanu::Numerology.new("William Wilberforce","24-08-1759")}
-  it "has a version number" do
+
+  it 'has a version number' do
     expect(Rumanu::VERSION).not_to be nil
   end
-  context "with a valid name" do
-    it 'is a string' do
-      expect(entity.name.is_a? String).to be true
+  context 'when alphabet is valid' do
+    let(:entity) {Rumanu::Numerology.new("John Lennon","09-10-1940")}
+    it 'is a Hash Object' do
+      expect(entity.alphabet.is_a? Hash).to be true
     end
-    it "has more than 5 characters" do
-      expect(entity.name.length).to be >= 5
+    it 'has at least one pair of key=>value' do
+      expect(entity.alphabet.empty?).to be false
     end
   end
-  it "raises an error if name is not a String"
-  it "raises and error if name is blank"
-  it "raises and error if name has less than 5 chars"
+  context 'when the name is valid' do
+    it "returns the name's vibration"
+  end
+  context 'when the name is invalid' do
+    let(:entity2) {Rumanu::Numerology.new("","09-10-1940")}
+    let(:entity3) {Rumanu::Numerology.new(nil,"24-08-1759")}
+    it 'return an error message if not string' do
+      expect{entity3.name}.to raise_error(ArgumentError)
+    end
+    it 'return an error message if string is empty' do
+      expect{entity2.name}.to raise_error(ArgumentError)
+    end
+
+  end
 end

ec22a68c1ed65488d22c23ab9965445fccd3e45c
Alberto Negron <albertonegron@gmail.com>
2019-08-14 21:16:18 +0100

    add meaning method

--- lib/rumanu/meaning.rb
+++ lib/rumanu/meaning.rb
@@ -1,6 +1,24 @@
-class Integer
-  def meaning
-    puts self + 10
+
+require 'yaml'
+module Rumanu
+  module Meaning
+    GEM_ROOT = File.expand_path("../..", __FILE__)
+    def load_meaning
+      YAML.load_file("#{GEM_ROOT}/rumanu/meanings/en.yml")
+    end
+
+    def meaning
+      case self
+      when 1..9
+        return load_meaning[self]
+      else
+        return "Value out of range. Please choose a number between 1 and 9"
+      end
+    end
   end
+end
+
+class Integer
+  include Rumanu::Meaning
 end
 
